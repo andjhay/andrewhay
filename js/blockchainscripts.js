c425f17adaaf596b66f2b03306d0aec29c639bb3
andjhay <88853764+andjhay@users.noreply.github.com>
2022-03-08 21:59:42 +0100

    go

--- js/blockchainscripts.js
+++ js/blockchainscripts.js
@@ -1,13 +1,3 @@
-const url = "https://eth-mainnet.alchemyapi.io/v2/AZL1qUMyWcVaiO3Z5w8nEYMdz_pStz-o";
-
-// Using web3js
-const web3 = new Web3(url);
-
-// Using ethers.js
-const provider = new ethers.providers.JsonRpcProvider(url);
-
-// <button class="enableEthereumButton">Enable Ethereum</button>
-
 const ethereumButton = document.querySelector(".enableEthereumButton");
 const showAccount = document.querySelector(".showAccount");
 
@@ -20,3 +10,5 @@ async function getAccount() {
   const account = accounts[0];
   showAccount.innerHTML = account;
 }
+
+// ethereum.isConnected(): boolean;

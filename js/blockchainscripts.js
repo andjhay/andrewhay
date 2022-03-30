fe672fddc0e0d62048300d805c510832ae105381
andjhay <88853764+andjhay@users.noreply.github.com>
2022-03-30 11:55:56 +0200

    k

--- js/blockchainscripts.js
+++ js/blockchainscripts.js
@@ -1,26 +1,18 @@
-const web3Installed = document.querySelector(".web3-compatible");
-const ethereumButton = document.querySelector(".enable-ethereum-button");
-const showAccount = document.querySelector(".show-account");
-const showChainId = document.querySelector(".show-chain-id");
-
 if (typeof window.ethereum !== "undefined") {
   console.log("MetaMask is installed!");
-  web3Installed.innerHTML = `<p> Web3 is installed </p>`;
 }
+
+const ethereumButton = document.querySelector(".enableEthereumButton");
+const showAccount = document.querySelector(".showAccount");
+const showBalance = document.querySelector(".showBalance");
 
 ethereumButton.addEventListener("click", () => {
   getAccount();
-  getChain();
 });
 
 async function getAccount() {
   const accounts = await ethereum.request({ method: "eth_requestAccounts" });
   const account = accounts[0];
   showAccount.innerHTML = account;
-}
-
-async function getChain() {
-  const chainId = await ethereum.request({ method: "eth_chainId" });
-  console.log(chainId);
-  showChainId.innerHTML = chainId;
+  return account;
 }
